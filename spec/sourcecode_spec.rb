32702bea9b6043a97602c3d336d0a19a8adf9552
Silas Barta <sbarta@gmail.com>
2012-04-20 23:37:50 -0700

    updated paths in tests and removed faulty git ref

--- spec/sourcecode_spec.rb
+++ spec/sourcecode_spec.rb
@@ -1,9 +1,13 @@
 require File.expand_path(File.dirname(__FILE__) + '/../lib/sourcecode')
 
+this_dir = File.dirname(__FILE__) + "/../"
+this_dir_expanded = File.expand_path(File.dirname(__FILE__) + "/../")
+puts this_dir
+puts this_dir_expanded
 
 describe "SourceCode"  do
     before :each do
-      @doc = SourceCode.new('./tests/test.rb', :file)
+      @doc = SourceCode.new('tests/test.rb', :file)
     end
     it "should create an array for storing a sourcecode file" do
       @doc.should be_an_instance_of(SourceCode)
@@ -20,12 +24,12 @@ describe "SourceCode"  do
       end
 
       it "should return methods followed by paranthesis as part of the #count_methods array" do
-        @doc = SourceCode.new('./tests/test_parens.rb')
+        @doc = SourceCode.new('tests/test_parens.rb')
         @doc.count_methods(@ruby_methods).should == {"sort_by"=>1, "each"=>2, "split"=>2}
       end
 
       it "should return an array of methods, ignoring anything commented." do
-        @doc = SourceCode.new('./tests/test_commenting.rb')
+        @doc = SourceCode.new('tests/test_commenting.rb')
         @doc.count_methods(@ruby_methods).should == {"reverse"=>1, "each"=>2, "split"=>1, "find"=>1}
       end
       
@@ -38,19 +42,19 @@ describe "SourceCode"  do
       end
       
       it "should parse out singe .'s " do
-        @doc = SourceCode.new('./tests/period_test.rb')
+        @doc = SourceCode.new('tests/period_test.rb')
         @doc.count_methods(@ruby_methods).should == {"reverse"=>1, "each"=>2, "split"=>1, "find"=>1}
       end
       
     end
     
-    describe "#from_git" do
-      it "should download copy of git source code into sub directory /repos/*name_of_repo" do
-        @ruby_methods = {'reverse'=>['string','hash','array'], 'split'=>['string'], 'each'=>['array', 'hash'], 'find'=>['array']}
-        doc = SourceCode.from_git('git://github.com/sharksforcheap/test.git')
-        doc.count_methods(@ruby_methods).should == {"reverse"=>1, "each"=>2, "split"=>1, "find"=>1}
-      end
-    end
+#    describe "#from_git" do
+#      it "should download copy of git source code into sub directory /repos/*name_of_repo" do
+#        @ruby_methods = {'reverse'=>['string','hash','array'], 'split'=>['string'], 'each'=>['array', 'hash'], 'find'=>['array']}
+#        doc = SourceCode.from_git('git://github.com/sharksforcheap/test.git')
+#        doc.count_methods(@ruby_methods).should == {"reverse"=>1, "each"=>2, "split"=>1, "find"=>1}
+#      end
+#    end
     
     describe "#from_file" do
       before :each do
@@ -58,7 +62,7 @@ describe "SourceCode"  do
       end
       
       it "should be able to instantiate through factory method on single file" do
-        code_obj = SourceCode.from_file('./tests/test.rb')
+        code_obj = SourceCode.from_file('tests/test.rb')
         code_obj.count_methods(@ruby_methods).should == {"reverse"=>1, "each"=>2, "split"=>1, "find"=>1}
       end
       
@@ -70,22 +74,22 @@ describe "SourceCode"  do
       end      
     
       it "should be able to instantiate through factory method on directory" do
-        code_obj = SourceCode.from_dir('./tests/test_input_directory/subdir')
+        code_obj = SourceCode.from_dir('tests/test_input_directory/subdir')
         code_obj.count_methods(@ruby_methods).should == {"reverse"=>1, "each"=>2, "split"=>1, "find"=>1}
       end
     
       it "should be able to instantiate through factory method on directory if given with final slash" do
-        code_obj = SourceCode.from_dir('./tests/test_input_directory/subdir/')
+        code_obj = SourceCode.from_dir('tests/test_input_directory/subdir/')
         code_obj.count_methods(@ruby_methods).should == {"reverse"=>1, "each"=>2, "split"=>1, "find"=>1}
       end
     
       it "should be able to instantiate through factory method on directory with subdirectories" do
-        code_obj = SourceCode.from_dir('./tests/test_input_directory')
+        code_obj = SourceCode.from_dir('tests/test_input_directory')
         code_obj.count_methods(@ruby_methods).should == {"reverse"=>3, "each"=>6, "split"=>3, "find"=>3}
       end
       
       it "should not interpret non-directories as directories" do
-        code_obj = SourceCode.from_dir('./tests/readme_test')
+        code_obj = SourceCode.from_dir('tests/readme_test')
         code_obj.count_methods(@ruby_methods).should == {"reverse"=>1, "each"=>2, "split"=>1, "find"=>1}
       end
       
